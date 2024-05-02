9bcdcf105953ef01cce81d7eb63a615cedeb911d
KStar-coder <sharmakaustav04@gmail.com>
2024-05-02 19:07:34 +0530

    Full webpage developed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { socials } from '../constants'
 
 const Footer = () => {
     return (
-        <Section crosses className="!px=0 !py-10">Footer
+        <Section crosses className="!px=0 !py-10">
 
             <div className='container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col'>
 
