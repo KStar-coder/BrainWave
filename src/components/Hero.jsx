187a8b2b9fa70abffb73e40e76508c3f713a8801
KStar-coder <sharmakaustav04@gmail.com>
2024-05-02 10:37:19 +0530

    Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
                             <img src={curve} className="absolute top-full left-0 w-full xl:=mt-2" width={624} height={28} alt="curve" />
                         </span>
                     </h1>
-                    <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 mb-8">
+                    <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
                         Unleash the power of AI within Brainwave. Upgrade your productivity with Brainwave, the open AI chat app.
                     </p>
 
@@ -63,7 +63,7 @@ const Hero = () => {
 
 
                                 <ScrollParallax isAbsolutelyPositioned>
-                                    <ul className="hidden absoulute -left-[5.5rem]
+                                    <ul className="hidden absolute -left-[5.5rem]
                                     bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
                                         {heroIcons.map((icon, index) => (
                                             <li className="p-5" key={index}>
@@ -101,7 +101,7 @@ const Hero = () => {
 
                 </div>
 
-                <CompanyLogos className="hidden relaticve z-10 mt-20 lg:block" />
+                <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
 
             </div>
             <BottomLine />
